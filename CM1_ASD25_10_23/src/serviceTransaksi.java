01da81a7f69f5cb7f67e24d4a05fac254c35555a
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2025-04-16 11:35:15 +0700

    find max searching rafi

--- CM1_ASD25_10_23/src/serviceTransaksi.java
+++ CM1_ASD25_10_23/src/serviceTransaksi.java
@@ -16,6 +16,29 @@ public class serviceTransaksi {
         }
     }
 
+    void FindMax() { //Menu 4 Cari Max Saldo
+        if (idx == 0) return;
+        transaksi max = trs[0];
+        for (int i = 1; i < idx; i++) {
+            if (trs[i].saldo > max.saldo) {
+                max = trs[i];
+            }
+        }
+        max.tampilDataTransaksi();
+    }
+
+    void searching(String noRek) { //Menu 3 Cari No rekening 
+        boolean found = false;
+        for (int i = 0; i < idx; i++) {
+            if (trs[i].bankAcc.noRekening.equalsIgnoreCase(noRek)) {
+                trs[i].tampilDataTransaksi();
+                found = true;
+            }
+        }
+        if (!found) System.out.println("No Rekening yang dicari tidak ditemukan.");
+    }
+
+
     void displayData() {
         for (int i = 0; i < idx; i++) {
             trs[i].tampilDataTransaksi();
