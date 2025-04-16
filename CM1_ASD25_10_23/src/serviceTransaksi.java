40d191e0cd4954b60dac88779dc977a4bb0a0f39
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2025-04-16 10:17:56 +0700

    sortingdsc rafi

--- CM1_ASD25_10_23/src/serviceTransaksi.java
+++ CM1_ASD25_10_23/src/serviceTransaksi.java
@@ -3,5 +3,20 @@ import java.util.Scanner;
 public class serviceTransaksi {
     transaksi[] trs = new transaksi[10];
     int idx = 0;
+    int jumData;
 
-}
+    public void sortingDSC() {
+        for (int i = 0; i < idx - 1; i++) {
+            int maxIdx = i;
+            for (int j = i + 1; j < idx; j++) {
+                if (trs[j].saldo > trs[maxIdx].saldo) {
+                    maxIdx = j;
+                }
+            }
+            transaksi temp = trs[i];
+            trs[i] = trs[maxIdx];
+            trs[maxIdx] = temp;
+        }
+    }
+    }
+
