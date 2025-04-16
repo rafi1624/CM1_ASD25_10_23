db391cdf126d5753276ebe97f31e04a3c61ef540
gaduhprakoso <gaduhprakoso@gmail.com>
2025-04-16 09:22:57 +0700

    gaduh bank atribut

---
+++ CM1_ASD25_10_23/src/bank.java
@@ -0,0 +1,25 @@
+class Bank {
+    String noRekening, nama, namaIbu, noHp, email;
+
+    
+    public Bank(){
+
+    }
+
+    public Bank(String noRekening, String nama, String namaIbu, String noHp, String email) 
+    {
+        this.noRekening = noRekening;
+        this.nama = nama;
+        this.namaIbu = namaIbu;
+        this.noHp = noHp;
+        this.email = email;
+    }
+    
+    // public void tampil() {
+    //     System.out.println("Kode : " + kode);  
+    //     System.out.println("Nama : " + nama);
+    //     System.out.println("Jenis Kelamin : " + (jenisKelamin ? "Laki-laki" : "Perempuan"));
+    //     System.out.println("Usia : " + usia);
+    //     System.out.println("");
+    // }
+}
