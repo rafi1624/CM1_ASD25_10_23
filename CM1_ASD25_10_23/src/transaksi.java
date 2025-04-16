e992e59522c30b9fa929cd063902841b3554f074
gaduhprakoso <gaduhprakoso@gmail.com>
2025-04-16 10:00:11 +0700

    transaksi gaduh

--- CM1_ASD25_10_23/src/transaksi.java
+++ CM1_ASD25_10_23/src/transaksi.java
@@ -1,3 +1,25 @@
 public class transaksi {
+    String KodeTransaksi, tanggalTransaksi, type;
+    double saldo, inOutSaldo, finalSaldo;
+    Bank bankAcc = new Bank();
+
+    public transaksi() {
+    
+    }
+    
+    public transaksi(String KodeTransaksi, double saldo, double inOutSaldo, double finalSaldo, String type, Bank bankAcc) {
+        this.KodeTransaksi = KodeTransaksi;
+        this.saldo = saldo;
+        this.inOutSaldo = inOutSaldo;
+        this.finalSaldo = finalSaldo;
+        this.type = type;
+        this.bankAcc = bankAcc;
+    }
+    
+    public void tampilDataTransaksi() {
+
+        System.out.println("Kode Transaksi \t No Rekening \t Saldo \t Debit/Kredit \t Final Saldo \t Tanggal Transaksi \t Type");
+        System.out.println(KodeTransaksi +"\t"+ bankAcc.noRekening +"\t"+ saldo +"\t"+ inOutSaldo +"\t"+ finalSaldo +"\t"+ tanggalTransaksi +"\t"+ type);
+    }
     
 }
